43a98c49f5fd6a1fbbeef34063dc33f53246c6fb
Jesse <jesseryan@bluewin.ch>
2023-03-14 20:55:03 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(express.json());
+const sqlite3 = require('sqlite3').verbose();
+
+app.listen(3001, function () {
+    console.log("Server started on port 3001");
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
