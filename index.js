adbc70935e4e171973575f6cb9ffc91b52dff886
Jutarokun <jesseryan@bluewin.ch>
2023-03-15 18:01:17 +0100

    made a fiew directory changes and minor code changes

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express();
 app.use(express.json());
 const sqlite3 = require('sqlite3').verbose();
 
+app.use(express.static('staticFiles'));
+
 app.listen(3001, function () {
     console.log("Server started on port 3001");
 });
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/html/index.html");
 });
